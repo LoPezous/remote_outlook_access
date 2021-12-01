ce193ed2b0972a1000c9bbde4312ad2a380b7576
Martin Pezous <66411147+LoPezous@users.noreply.github.com>
2021-12-01 19:49:41 +0100

    Add files via upload

---
+++ t.py
@@ -0,0 +1,12 @@
+import win32com.client as win32
+adresses = ['martin.pezous-puech@live.fr']
+for adress in adresses:
+
+        outlook = win32.Dispatch('Outlook.Application')
+        mail = outlook.CreateItem(0)
+        mail.To = adress
+        mail.Subject = 'Analysis: Done'
+        mail.Body = ''
+        mail.Attachments.Add(r'C:\Users\marti\Desktop\signature.png')
+        mail.HTMLBody = ''
+        mail.Send()
